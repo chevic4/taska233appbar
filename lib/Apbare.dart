529e7879aaf90d8c61fb31490641e0af58ef4a7b
chevic <victor.chinenov@gmail.com>
2022-05-15 16:40:54 +0300

    formating in dart format

--- lib/Apbare.dart
+++ lib/Apbare.dart
@@ -13,75 +13,76 @@ class _AppbarScreenState extends State<AppbarScreen> {
     final ButtonStyle buttonStyle =
         TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('панель AppBar'),
-        actions: <Widget>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.access_alarm)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.account_box)),
-          TextButton(
-            style: buttonStyle,
-            onPressed: () {},
-            child: const Text('кнопка'),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'панель NavigationDrawer',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              title: const Text('первый пункт'),
-              leading: const Icon(Icons.access_alarm_sharp),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-              indent: 10,
-              endIndent: 10,
-              color: Colors.black,
-            ),
-            ListTile(
-              onTap: () {},
-              title: const Text('второй пункт'),
-              leading: const Icon(Icons.access_alarm_sharp),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-              indent: 10,
-              endIndent: 10,
-              color: Colors.black,
-            ),
-            ListTile(
-              onTap: () {},
-              title: const Text('третий пункт'),
-              leading: const Icon(Icons.access_alarm_sharp),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-              indent: 10,
-              endIndent: 10,
-              color: Colors.black,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('панель AppBar'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.access_alarm)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.account_box)),
+            TextButton(
+              style: buttonStyle,
+              onPressed: () {},
+              child: const Text('кнопка'),
             ),
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'панель NavigationDrawer',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text('первый пункт'),
+                leading: const Icon(Icons.access_alarm_sharp),
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+                color: Colors.black,
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text('второй пункт'),
+                leading: const Icon(Icons.access_alarm_sharp),
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+                color: Colors.black,
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text('третий пункт'),
+                leading: const Icon(Icons.access_alarm_sharp),
+              ),
+              const Divider(
+                height: 1,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ),
+        body: const Center(child: Text('контекст')),
       ),
-      body: Center(child: Text('контекст')),
-    ));
+    );
   }
 }
--- lib/navidro.dart
+++ lib/navidro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class NaviDra extends StatefulWidget {
   const NaviDra({Key? key}) : super(key: key);
 
@@ -9,12 +10,10 @@ class NaviDra extends StatefulWidget {
 class _NaviDraState extends State<NaviDra> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-
         body: Center(child: Text('пример')),
-      )
+      ),
     );
   }
 }
-
